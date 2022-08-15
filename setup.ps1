23ef6ff36c580fa9591778b09e03643bc46b3de7
Robert Hawk <robert@the-hawk.us>
2022-08-15 13:57:27 -0600

    Some changes to stuff

--- setup.ps1
+++ setup.ps1
@@ -3,6 +3,7 @@ $TEXMFHOME = Join-Path $(kpsewhich -var-value TEXMFHOME) -ChildPath tex
 # Packages
 New-Item -Path "${TEXMFHOME}\ag7ifform.sty" -ItemType SymbolicLink -Value .\sty\ag7ifform.sty 
 New-Item -Path "${TEXMFHOME}\ag7ifpub.sty" -ItemType SymbolicLink -Value .\sty\ag7ifpub.sty 
+New-Item -Path "${TEXMFHOME}\bibleref-ag7if.sty" -ItemType SymbolicLink -Value .\sty\bibleref-ag7if.sty 
 New-Item -Path "${TEXMFHOME}\fouo.sty" -ItemType SymbolicLink -Value .\sty\fouo.sty 
 New-Item -Path "${TEXMFHOME}\infosec.sty" -ItemType SymbolicLink -Value .\sty\infosec.sty 
 New-Item -Path "${TEXMFHOME}\tlp.sty" -ItemType SymbolicLink -Value .\sty\tlp.sty 
@@ -14,5 +15,6 @@ New-Item -Path "${TEXMFHOME}\ag7ifcard.cls" -ItemType SymbolicLink -Value .\cls\
 New-Item -Path "${TEXMFHOME}\ag7ifform.cls" -ItemType SymbolicLink -Value .\cls\ag7ifform.cls 
 New-Item -Path "${TEXMFHOME}\ag7ifref.cls" -ItemType SymbolicLink -Value .\cls\ag7ifref.cls 
 New-Item -Path "${TEXMFHOME}\ag7ifrfc.cls" -ItemType SymbolicLink -Value .\cls\ag7ifrfc.cls 
+New-Item -Path "${TEXMFHOME}\oplan.cls" -ItemType SymbolicLink -Value .\cls\oplan.cls 
 New-Item -Path "${TEXMFHOME}\usafmemo.cls" -ItemType SymbolicLink -Value .\cls\usafmemo.cls 
 New-Item -Path "${TEXMFHOME}\usafpaper.cls" -ItemType SymbolicLink -Value .\cls\usafpaper.cls 
